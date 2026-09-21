12b20a5307b8c0ef949fe77598e4b8f8c117f33d
jbs695-gif <jbs695@nyu.edu>
2026-09-21 15:07:08 -0400

    classwork

---
+++ week3/week03_p5PolarTemplate/p5PolarTemplate/sketch.js
@@ -0,0 +1,14 @@
+// p5Polar Template
+// https://github.com/liz-peng/p5.Polar
+// https://liz-peng.github.io/p5.Polar/
+// Review the index.html for the <script></script> 
+
+function setup() {
+  createCanvas(500, 500);
+}
+
+function draw() {
+  background(255,0,0);
+  
+  // Insert your drawing here
+}
